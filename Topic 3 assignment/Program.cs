a474a60735a461631793e34383b1d1fed3cb767d
Raine DeVries <362571390@GoTVDSB.ca>
2024-09-10 09:37:45 -0400

    Add project files.

---
+++ Topic 3 assignment/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Raine DeVries
+int intel = 29;
+double price = 2.99;
+string computerScience = ("I am learning a bit of Computer Science!");
+
+Console.WriteLine(intel);
+Console.WriteLine(price.ToString("C"));
+Console.WriteLine(computerScience);
+Console.WriteLine("Press ENTER");
+Console.ReadLine();
+Console.Clear();
+
+string name = ("My name is Raine DeVries");
+string time = (" i'll graduate in ");
+int year = 2027;
+
+Console.WriteLine(name + time + year);
+Console.WriteLine("Press ENTER");
+Console.ReadLine();
+
+
+double radius = (25 * Math.PI);
+Console.WriteLine("The radius of a circle that is 25 inches in diamater is");
+Console.WriteLine(Math.Round(radius));
+Console.ReadLine();
+
+
